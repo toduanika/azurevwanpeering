33934f5e70a2e5909aaf574febe8a7182df58ae9
Nika Todua <nika_todua@epam.com>
2024-07-19 17:24:38 +0400

    update readme and add media files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# azurevwanpeering
+# Azure Virtual WAN - Automate peering from Virtual Hubs to Virtual Networks
+
+Networking is one of the most critical aspects of Azure: without it, different components and workloads cannot interconnect. Azure offers several ways to plan your network infrastructure. This planning involves deciding on the geographical location of different components/workloads, which in networking terms is referred to as topology.
+
+Azure provides a resource called Azure Virtual WAN for a managed hub/spoke topology. In this setup, the main connectivity point or virtual hub within each region is managed by Azure (although technically, it remains a virtual network). This regional virtual hub is used to connect the virtual networks of different projects or workloads within the corresponding virtual hub using peering connections. While Azure does make global VNet peering possible, ideally, networks in the same region should connect with each other.
+
+![Virtual Hub to Virtual Network](/media/vhub-regional-peering.drawio.png)
---
+++ automateVhubsAndVnetsPeering.ps1
@@ -0,0 +1,59 @@
+<#
+    .SYNOPSIS
+    This script creates peering connection from virtual hubs to virtual networks based on location.
+
+    .DESCRIPTION
+    This script collects virtual hubs and virtual networks existing in each subscription, then checks their locations and creates the corresponding peering connections.
+
+    .PARAMETER resourceGroupName
+    Specifies the existing resource group name whete virtual wan has been deployed. This parameter is mandatory and should not be empty
+
+    .NOTES
+        Author          : Nika Todua.
+
+#>
+
+[CmdLetBinding()]
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [System.String] $resourceGroupName
+)
+
+$resourceGroup = Get-AzResourceGroup -ResourceGroupName $resourceGroupName
+$virtualHubs = Get-AzVirtualHub -ResourceGroupName $resourceGroup.ResourceGroupName
+
+if ($virtualHubs -ne 0) {
+
+    # create peering for each virtual hub, based on environment and location
+    foreach ($virtualHub in $virtualHubs) {
+
+        $subscriptions = Get-AzSubscription
+
+        foreach ($subscription in $subscriptions) {
+            Select-AzSubscription -SubscriptionName $subscription.Name
+
+            $virtualNetworks = Get-AzVirtualNetwork
+
+            if ($virtualNetworks.Count -ne 0) {
+
+                foreach ($virtualNetwork in $virtualNetworks) {
+                    $virtualNetworkConnectionName = $subscription.Name + '-' + $virtualNetwork.ResourceGroupName + '-' + $virtualNetwork.Name
+
+                    if ($virtualNetwork.Location -eq $virtualHub.Location) {
+
+                        New-AzVirtualHubVnetConnection -ResourceGroupName $resourceGroup.ResourceGroupName -VirtualHubName $virtualHub.Name -Name $virtualNetworkConnectionName -RemoteVirtualNetwork $virtualNetwork
+                    }
+                    
+                }
+
+            }
+                
+        }
+        
+    }
+
+}
+else {
+    Write-Information -MessageData "Virtual Hubs have not been found."
+}
